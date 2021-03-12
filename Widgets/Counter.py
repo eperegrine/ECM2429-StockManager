febfd02e39e2cab64ffcb2597108185d322b105d
Emily Peregrine <emily.peregrine1@nhs.net>
2021-03-12 11:07:25 +0000

    Initial Commit with test project

---
+++ Widgets/Counter.py
@@ -0,0 +1,17 @@
+from kivy.app import App, Builder
+from kivy.uix.widget import Widget
+
+Builder.load_file("Widgets/Counter.kv")
+
+class Counter(Widget):
+
+    def on_kv_post(self, base_widget):
+        self.update_counter()
+
+    def increment(self):
+        self.count += 1
+        self.update_counter()
+
+    def update_counter(self):
+        count_lbl = self.ids['count_lbl']
+        count_lbl.text = f"Counter: {self.count}"
---
+++ main.py
@@ -0,0 +1,18 @@
+from kivy.app import App, Builder
+from kivy.uix.widget import Widget
+from Widgets import Counter
+
+Builder.load_file("main.kv")
+
+class MainApp(Widget):
+    pass
+
+class StockManagerApp(App):
+    count = 0
+
+    def build(self):
+        layout = MainApp()
+        return layout
+
+if __name__ == '__main__':
+    StockManagerApp().run()
