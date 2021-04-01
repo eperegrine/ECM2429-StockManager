812f8cb9b0851f7b227148c799987c9714ab1388
Emily Peregrine <emily.peregrine1@nhs.net>
2021-04-01 11:19:44 +0100

    Removed SQL files

--- SQLScripts/StockLevels.sql
+++
@@ -1,8 +0,0 @@
-SELECT SI.location, P.name, SI.quantity, P.target_level
-FROM StockItems SI
-         LEFT JOIN Products P on P.id = SI.product_id;
-
-SELECT P.name, SUM(SI.quantity) AS "stock_level", P.target_level
-FROM StockItems SI
-         LEFT JOIN Products P on P.id = SI.product_id
-GROUP BY P.name;
