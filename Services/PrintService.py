8c395999c502d8338e292e3327ae8a78b7dfb55b
Emily Peregrine <emily.peregrine1@nhs.net>
2021-04-02 16:01:48 +0100

    Fixed pep8 issues

--- Services/PrintService.py
+++ Services/PrintService.py
@@ -17,20 +17,20 @@ def _sys_open_file(filepath):
 
     :param filepath: The path of the file to open
     """
-    if platform.system() == 'Darwin':       # macOS
+    if platform.system() == 'Darwin':  # macOS
         subprocess.call(('open', filepath))
-    elif platform.system() == 'Windows':    # Windows
+    elif platform.system() == 'Windows':  # Windows
         os.startfile(filepath)
-    else:                                   # linux variants
+    else:  # linux variants
         subprocess.call(('xdg-open', filepath))
 
 
 class PDF(FPDF):
     def add_title(self, text):
-        self.set_xy(0.0,0.0)
+        self.set_xy(0.0, 0.0)
         self.set_font('Arial', 'B', 16)
         self.set_text_color(220, 50, 50)
-        self.cell(w=297.0/2, h=40.0, align='C', txt=text, border=0)
+        self.cell(w=297.0 / 2, h=40.0, align='C', txt=text, border=0)
 
 
 class AddressLabelPDF(FPDF):
@@ -40,28 +40,28 @@ class AddressLabelPDF(FPDF):
         self.set_font('Arial', 'B', 16)
         pad_x = 5
         line_pad = 5
-        start_height=10
+        start_height = 10
         self.text(pad_x, start_height, "Ship To:")
         self.set_font('Arial', '', 14)
         current_line = start_height + line_pad + 2
         self.text(pad_x, current_line, order.customer_name)
         address_lines = order.address.split(",")
         for index, line in enumerate(address_lines):
-            current_line = 23 + line_pad*index
+            current_line = 23 + line_pad * index
             self.text(pad_x, current_line, line.strip())
 
         shipping_text = f"Shipping Ref: MADEUPFORTEST"
         width = self.get_string_width(shipping_text)
-        half_pad = line_pad/2
+        half_pad = line_pad / 2
         sep_line_y = current_line + (half_pad * 1.5)
-        self.line(half_pad, sep_line_y, width + pad_x*2+half_pad, sep_line_y)
-        current_line = current_line + line_pad*2
+        self.line(half_pad, sep_line_y, width + pad_x * 2 + half_pad, sep_line_y)
+        current_line = current_line + line_pad * 2
         self.text(pad_x, current_line, f"Shipping Ref: MADEUPFORTEST")
         sep_line_y = current_line + (half_pad * 1.5)
-        self.line(half_pad, sep_line_y, width + pad_x*2+half_pad, sep_line_y)
-        current_line = current_line + line_pad*2
+        self.line(half_pad, sep_line_y, width + pad_x * 2 + half_pad, sep_line_y)
+        current_line = current_line + line_pad * 2
         self.text(pad_x, current_line, f"From ACME corp via {order.storefront}")
-        self.rect(half_pad, half_pad, width + pad_x*2, current_line)
+        self.rect(half_pad, half_pad, width + pad_x * 2, current_line)
 
 
 class PackingListPDF(FPDF):
@@ -71,7 +71,7 @@ class PackingListPDF(FPDF):
         self.set_font('Arial', 'B', 16)
         pad_x = 5
         line_pad = 7
-        start_height=10
+        start_height = 10
         self.text(pad_x, start_height, f"ORDER: #{order.id:04d}")
         self.set_font('Arial', '', 14)
         current_line = start_height + line_pad + 2
@@ -87,10 +87,10 @@ class PackingListPDF(FPDF):
             self.text(pad_x, current_line, line.strip())
 
         products = order.products
-        current_line += line_pad*1.5
+        current_line += line_pad * 1.5
         self.set_font('Arial', 'b', 14)
         self.text(pad_x, current_line, f"Products:")
-        current_line += line_pad/4
+        current_line += line_pad / 4
         self.set_font('Arial', '', 14)
         for po in products:
             current_line += line_pad
@@ -107,7 +107,7 @@ class PrintService:
             path = path[0]
         print(path)
         output_file = os.path.join(path, filename)
-        output_file += f"{''.join( str(time.time()).split('.'))}.pdf"
+        output_file += f"{''.join(str(time.time()).split('.'))}.pdf"
         return output_file
 
     def print_order_address_label(self, order: OrderDalModel):
